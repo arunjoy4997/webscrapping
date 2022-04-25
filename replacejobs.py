f67f741cf0a4ef61c73d690d13994beeed4eef57
arunjoy4997 <103504327+arunjoy4997@users.noreply.github.com>
2022-04-25 18:48:40 +0530

    Add files via upload

--- replacejobs.py
+++ replacejobs.py
@@ -11,23 +11,28 @@ location=[]
 joburl=[]
 
 for i in range(24):
-    jobname.append(text.iloc[:,:1])
-    company.append(text.iloc[:,1:2])
-    location.append(text.iloc[:,2:3])
-    joburl.append(text.iloc[:,3:4])
+    jobname.append(text.iloc[i:i+1,:1])
+    company.append(text.iloc[i:i+1,1:2])
+    location.append(text.iloc[i:i+1,2:3])
+    joburl.append(text.iloc[i:i+1,3:4])
 
-
+print(jobname[0])
 
 base = os.path.dirname(os.path.abspath(__file__))
 html = open(os.path.join(base, 'datanuggets.html'))
 soup=BeautifulSoup(html,'html.parser')
 
-old_text = soup.find('div',class_='cardx')
-new_text=old_text.find(text=re.compile('Sr Data Scientist')).replace_with('hello world')
-# new_text=old_text.find(text=re.compile('Principal Data Scientist')).replace_with('hello mahhhn')
+jobs=[]
+for i in range(1,6):
+    job = soup.find('div',class_='cardx'+str(i)).h3.get_text(strip=True)
+    jobs.append(job)
+# print(jobs)
 
-# for i in soup.find('div',class_='cardx'):
-#     i.find(text=re.compile('Sr Data Scientist')).replace_with('hello world')
+
+for i in range(1,6):
+    old_text = soup.find('div',class_='cardx'+str(i)).h3
+    new_text=old_text.find(text=re.compile(str(jobs[i-1]))).replace_with(str(jobname[i-1]))
+
 
 with open("datanuggets1.html", "wb") as f_output:
     f_output.write(soup.prettify("utf-8"))
